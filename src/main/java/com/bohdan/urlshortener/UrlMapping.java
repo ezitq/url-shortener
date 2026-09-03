942957b48a7507a9c6d995614daed1a1ba8aee4b
ezitq <bkgksnec@gmail.com>
2026-09-03 18:37:07 +0300

    feat: add UrlMapping MongoDB document model

---
+++ src/main/java/com/bohdan/urlshortener/UrlMapping.java
@@ -0,0 +1,35 @@
+package com.bohdan.urlshortener;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collation = "urls")
+public class UrlMapping {
+
+    @Id
+    private String id;
+
+    private String originalUrl;
+
+    @Indexed(unique = true)
+    private String shortCode;
+
+    @Builder.Default
+    private long clickCount = 0;
+
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private LocalDateTime expiresAt;
+}
